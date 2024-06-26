c91b80496e3cbe45e13eee1b1b9bc802bee87990
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-06-26 08:01:55 -0400

    Update checkPackages.py

--- Releases/windows/x64/AMD/internal/checkPackages.py
+++ Releases/windows/x64/AMD/internal/checkPackages.py
@@ -3,3 +3,10 @@ try:
     import PIL
 except ImportError:
     print("No pillow library found!")
+    os.system("python -m pip install pillow")
+
+try:
+    import cv2
+except ImportError:
+    print("No OpenCV library found!")
+    os.system("python -m pip install opencv-python")
