9a839fa546091cf4068c9d9d5ea846e337e72930
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-07-04 10:11:08 -0400

    Update program.bat

--- Releases/windows/x64/AMD/program.bat
+++ Releases/windows/x64/AMD/program.bat
@@ -2,3 +2,12 @@
 echo This is incomplete and is in testing. Do not use.
 cd internal
 zluda.exe DeviceSelecter.exe
+for /f %%i in ('python -c print("pythonpy",end="")') do (
+  SET x=%%i
+)
+if (%x == "pythonpy") (
+  echo python detected!
+)
+else (
+  echo no phthon found
+)
