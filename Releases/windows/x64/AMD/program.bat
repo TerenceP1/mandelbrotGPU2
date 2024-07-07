b0f02b85b528ac1ef1410a2192bc48c8dedd9213
Terence Peng <terencepps11@outlook.com>
2024-07-07 17:50:55 -0400

    THE SYNCRONIZER 3000

--- Releases/windows/x64/AMD/program.bat
+++ Releases/windows/x64/AMD/program.bat
@@ -2,12 +2,3 @@
 echo This is incomplete and is in testing. Do not use.
 cd internal
 zluda.exe DeviceSelecter.exe
-for /f %%i in ('python -c print("pythonpy",end="")') do (
-  SET x = %%i
-)
-if ("%x%" == "pythonpy") (
-  echo Python detected!
-)
-else (
-  echo No phthon found
-)
